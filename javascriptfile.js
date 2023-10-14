f8a068591327991d563528544906ad55a578f3db
Anushdeshmukh5 <147938679+Anushdeshmukh5@users.noreply.github.com>
2023-10-14 22:28:03 +0530

    Initial commit

---
+++ javascriptfile.js
@@ -0,0 +1,36 @@
+let btn = document.querySelector("#btn");
+let help = document.querySelector("#help");
+let customer = document.querySelector("#customer");
+let prod = document.querySelector("#prod");
+let income = document.querySelector("#income");
+let sidebar = document.querySelector(".sidebar");
+let searchBtnz = document.querySelector(".search");
+
+btn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
+searchBtn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
+
+
+function myFunction() {
+    prompt("How can we help you?");
+}
+function myFunction1() {
+    alert("65% new customers")
+    custo.style.color="blue";
+    custo.style.textDecoration="underline black";
+}
+
+function myFunction2() {
+    ps.style.color="blue";
+    ps.style.textDecoration="underline black";
+    ps.blink()
+}
+
+function myFunction3() {
+    earn.style.color="blue";
+    earn.style.textDecoration="underline black";
+
+}
